f241be99efdc790898a616b774815fc2e074a2b9
Mikhail Oleynik <mishutka@mito-team.com>
2025-01-09 17:03:04 +0300

    RenderCount() and RenderTableCount() methods added

--- helpers.go
+++ helpers.go
@@ -2,6 +2,29 @@ package mtweb
 
 import "github.com/mitoteam/dhtml"
 
+// Count label
+func RenderCount(count int64, title string) (out dhtml.HtmlPiece) {
+	out.Append(
+		Icon("abacus").Label(count).ElementClass("p-1 border border-dark-subtle fw-bold bg-info-subtle").Title(title),
+	)
+
+	return out
+}
+
+// Bootstrap styled table
 func NewTable() *dhtml.TableElement {
 	return dhtml.NewTable().Class("table table-hover table-sm").EmptyLabel("nothing here yet")
+}
+
+// Table count label
+func RenderTableCount(table *dhtml.TableElement, title string) (out dhtml.HtmlPiece) {
+	cnt := int64(table.RowCount())
+
+	if cnt > 0 {
+		out.Append(
+			dhtml.Div().Class("text-end").Append(RenderCount(cnt, title)),
+		)
+	}
+
+	return out
 }
