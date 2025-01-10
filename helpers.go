e3a4727b983dcbf614c41a4e32177e17d5d43cc5
Mikhail Oleynik <mishutka@mito-team.com>
2025-01-10 17:20:33 +0300

    TimestampElement added

--- helpers.go
+++ helpers.go
@@ -33,11 +33,57 @@ func RenderTableCount(table *dhtml.TableElement, title string) (out dhtml.HtmlPi
 	return out
 }
 
+// ================== timestamp element ====================
 // Clock icon with datetime
-func RenderTimestamp(ts time.Time) (out dhtml.HtmlPiece) {
-	out.Append(
-		Icon("clock").Label(ts.Format(time.DateTime)).ElementClass("small text-muted"),
-	)
+type TimestampElement struct {
+	format  string
+	icon    string
+	ts      time.Time
+	classes dhtml.Classes
+}
 
-	return out
+// force interface implementation declaring fake variable
+var _ dhtml.ElementI = (*TimestampElement)(nil)
+
+func NewTimestamp(ts time.Time) *TimestampElement {
+	return &TimestampElement{
+		ts:     ts,
+		icon:   "clock",
+		format: time.DateTime,
+	}
+}
+
+// Element classes.
+func (e *TimestampElement) Class(v ...any) *TimestampElement {
+	e.classes.Add(v...)
+	return e
+}
+
+func (e *TimestampElement) SmallMuted() *TimestampElement {
+	e.classes.Add("small", "text-muted")
+	return e
+}
+
+// Set date and time format (default is time.DateTime).
+func (e *TimestampElement) Format(tsFormat string) *TimestampElement {
+	e.format = tsFormat
+	return e
+}
+
+// icon name. empty = no icon. default "clock".
+func (e *TimestampElement) Icon(icon string) *TimestampElement {
+	e.icon = icon
+	return e
+}
+
+func (e *TimestampElement) GetTags() dhtml.TagList {
+	rootTag := dhtml.Div().Class(e.classes)
+
+	if e.icon != "" {
+		rootTag.Append(Icon(e.icon).Label(e.ts.Format(e.format)))
+	} else {
+		rootTag.Append(e.ts.Format(e.format))
+	}
+
+	return rootTag.GetTags()
 }
